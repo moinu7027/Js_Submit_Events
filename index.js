81abfb17f2945a09b1d4687a5bb21de1f38edf7c
Moin Uddin <89659537+moinu7027@users.noreply.github.com>
2024-09-19 12:33:40 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const form =document.querySelector("form");
+const name =document.querySelector("div #name");
+const email =document.querySelector("div #email");
+const password =document.querySelector("div #password");
+form.addEventListener("submit",formHandeler);
+
+function formHandeler(e){
+  e.preventDefault();
+  const userInfo ={
+    name :name.value,
+    email:email.value,
+    passwoord:password.value,
+    
+
+  };
+  console.log(userInfo);
+  name.value="";
+  email.value="";
+ password.value="";
+}
